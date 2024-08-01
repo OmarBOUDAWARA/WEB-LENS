6f603832501fe699947e7a28793f13c46b09b2ec
OmarBOUDAWARA <o.boudawara@habiba.tech>
2024-08-01 12:37:38 +0100

    3rd

--- examples/react-native/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@lens-protocol', '@react-native'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-};
